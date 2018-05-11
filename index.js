54bd9243fdce6808ed4484095e06f1121592076f
niuDazhuang <dengwm666@icloud.com>
2018-05-11 20:17:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   component: {
     install: (Vue, opts = { pre: 'Dt' }) => {
       const { pre } = opts
-      const requireComponent = require.context('./components', true, /\.vue$/)
+      const requireComponent = require.context('@/components', true, /\.vue$/)
       requireComponent.keys().forEach(key => {
         const componentName = pre + /^\S+\/(\w+).vue$/.exec(key)[1]
         const comConfig = requireComponent(key)
